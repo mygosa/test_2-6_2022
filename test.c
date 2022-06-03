fadaf5977605521dc50588033e13d456e91ae341
Administrator <Administrator@Win102022CICJOD>
2022-06-03 21:42:24 +0800

    添加项目文件。

---
+++ test.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define MAX 1000
+
+#include<stdio.h>	
+#include<Windows.h>
+#include<string.h>
+
+//int main()
+//
+//{
+//	
+//	extern fine;
+//	const int a = 100;
+//	int add = MAX;
+//	//scanf("%d", &add);
+//	printf("%d\n", add);
+//	printf("%d\n", a);
+//	printf("%d\n", fine);
+//	system("pause");
+//	return 0;
+//	
+//
+//}
+//enum sex
+//{
+//	men=2,
+//	wumen,
+//	huwumen
+//};
+//int main()
+//{
+//	enum sex s = men;
+//	printf("%d\n", men);
+//	printf("%d\n", wumen);
+//	printf("%d\n", huwumen);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[] = "hello";
+//	printf("%s", arr);
+//
+//	return 0;
+//}
+int main()
+{
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c'};
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", strlen(arr2));
+	//Çó×Ö·û´®³¤¶È
+	//int len=strlen("abc");
+	//printf("%d\n", len);
+	//printf("%s\n", arr1);
+	//printf("%s\n", arr2);
+
+	return 0;
+}
